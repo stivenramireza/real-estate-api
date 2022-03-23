c05431b229359e1c7af485cbb5bd1f059141c78c
Stiven Ramírez Arango <sramirez@superfuds.com.co>
2022-03-23 17:00:59 -0500

    [IMP] Filters by status, year and city

--- src/repositories/property_repository.py
+++ src/repositories/property_repository.py
@@ -1,5 +1,10 @@
 from src.repositories.database_repository import db_connection, DatabaseRepository
-from src.utils.queries import PROPERTIES, PROPERTIES_WITH_FILTERS
+from src.utils.queries import (
+    PROPERTIES,
+    STATUS_FILTER,
+    YEAR_FILTER,
+    CITY_FILTER,
+)
 
 
 class PropertyRepository:
@@ -7,14 +12,20 @@ class PropertyRepository:
     database_repository = DatabaseRepository()
 
     def get_properties(self, status: str, year: int, city: str) -> list[dict[str, any]]:
+        properties_to_search = [PROPERTIES]
+
         if status or year or city:
-            properties_to_search = f"{PROPERTIES}{PROPERTIES_WITH_FILTERS}"
-            values = (status, year, city)
+            if status:
+                properties_to_search.append(STATUS_FILTER.format(status))
+            if year:
+                properties_to_search.append(YEAR_FILTER.format(year))
+            if city:
+                properties_to_search.append(CITY_FILTER.format(city))
+            properties_to_search = " AND ".join(properties_to_search)
         else:
             properties_to_search = PROPERTIES
-            values = ()
 
         properties = self.database_repository.read_from_db(
-            properties_to_search, values, db_connection
+            properties_to_search, db_connection
         )
         return properties
